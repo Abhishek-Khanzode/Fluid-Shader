721d2e1490f948f4c79521d5c188acf981b09fdb
Abhishek-Khanzode <abhi.geek2004@gmail.com>
2026-04-25 08:34:12 +0530

    added changes in model.tsx fixed the threejs imports

--- app/components/Model.tsx
+++ app/components/Model.tsx
@@ -1,31 +1,32 @@
 'use client'
 
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment, Stats, useGLTF } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
 import React, { useRef } from 'react'
-import { degToRad } from 'three/src/math/MathUtils'
-import { Bloom, EffectComposer } from '@react-three/postprocessing'
+import { MathUtils } from 'three'
+import { EffectComposer } from '@react-three/postprocessing'
 import { Fluid } from '@whatisjery/react-fluid-distortion'
-
 
 const Model = () => {
     const meshRef = useRef<THREE.Mesh>(null!)
 
     useFrame(() => {
         if (meshRef.current) {
-            meshRef.current.rotation.x += 0.01
-            meshRef.current.rotation.y += 0.01
+            meshRef.current.rotation.x += MathUtils.degToRad(1)
+            meshRef.current.rotation.y += MathUtils.degToRad(1)
         }
     })
 
     return (
         <mesh ref={meshRef}>
             <boxGeometry args={[2, 2, 2]} />
-            <meshStandardMaterial color="#00ffff"
+            <meshStandardMaterial
+                color="#00ffff"
                 emissive="#00ffff"
                 emissiveIntensity={2}
                 transparent
-                opacity={0.7} />
+                opacity={0.7}
+            />
         </mesh>
     )
 }
@@ -33,14 +34,12 @@ const Model = () => {
 const Stone = () => {
     return (
         <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
-            <Environment preset='studio' environmentIntensity={1} />
+            <Environment preset="studio" environmentIntensity={1} />
 
             <Model />
 
-            {/* <Stats /> */}
-
             <EffectComposer>
-                <Fluid fluidColor='#1b1b1b' curl={30} />
+                <Fluid fluidColor="#1b1b1b" curl={30} />
             </EffectComposer>
         </Canvas>
     )
